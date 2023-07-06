6bc21261a7c0ac4c529e80c960fbc0f4626fda47
Abhay Sharma <85064231+Abhay31@users.noreply.github.com>
2023-07-06 16:34:11 +0530

    Fair Playoff

---
+++ Rating - 800/04. Fair Playoff.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>> t;
+    while(t--)
+    {
+        int s1,s2,s3,s4;
+        cin>>s1>>s2>>s3>>s4;
+        if(max(s1,s2)<min(s3,s4) || max(s3,s4)<min(s1,s2))
+        {
+            cout<<"No"<<endl;
+        }
+        else
+        {
+            cout<<"Yes"<<endl;
+        }
+    }
+}
